f1c55e1a10155fd43c92424d97f0ec0f83f494ac
moktadir <mirmoktadir@gmail.com>
2024-05-23 06:04:00 +0600

    final

--- lib/app/modules/home/views/movie_detail_view.dart
+++ lib/app/modules/home/views/movie_detail_view.dart
@@ -22,169 +22,171 @@ class MovieDetailView extends GetView<HomeController> {
         title: Text(movie?.title ?? ""),
         centerTitle: true,
       ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          SizedBox(height: 5.sp),
-          SizedBox(
-            width: double.infinity,
-            height: 250.sp,
-            child: NetworkImageBox(
-                url: "${ApiUrl.image}${movie?.posterPath}", radius: 0),
-          ),
-          Padding(
-            padding: EdgeInsets.all(18.sp),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movie?.title ?? "",
-                  style: theme.textTheme.headlineMedium,
-                ),
-                SizedBox(height: 12.sp),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      IconlyLight.time_circle,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 12.sp),
-                    Text(
-                      "${movie!.runtime.toString()} minutes",
-                      style: theme.textTheme.bodyLarge,
-                    ),
-                    SizedBox(width: 30.sp),
-                    const Icon(
-                      IconlyBold.star,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 12.sp),
-                    Text(
-                      "${movie.voteAverage?.toStringAsFixed(1)} iMDB",
-                      style: theme.textTheme.bodyLarge,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 18.sp),
-                Row(
+      body: Obx(() => ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              SizedBox(height: 5.sp),
+              SizedBox(
+                width: double.infinity,
+                height: 250.sp,
+                child: NetworkImageBox(
+                    url: "${ApiUrl.image}${movie?.posterPath}", radius: 0),
+              ),
+              Padding(
+                padding: EdgeInsets.all(18.sp),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Text(
+                      movie?.title ?? "",
+                      style: theme.textTheme.headlineMedium,
+                    ),
+                    SizedBox(height: 12.sp),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
+                        const Icon(
+                          IconlyLight.time_circle,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 12.sp),
                         Text(
-                          "Release date",
+                          "${movie!.runtime.toString()} minutes",
                           style: theme.textTheme.bodyLarge,
                         ),
-                        SizedBox(height: 10.sp),
+                        SizedBox(width: 30.sp),
+                        const Icon(
+                          IconlyBold.star,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 12.sp),
                         Text(
-                          "${movie.releaseDate}",
+                          "${movie.voteAverage?.toStringAsFixed(1)} iMDB",
                           style: theme.textTheme.bodyLarge,
                         ),
                       ],
                     ),
-                    const SizedBox(width: 100),
-                    Column(
+                    SizedBox(height: 18.sp),
+                    Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          "Genre",
-                          style: theme.textTheme.bodyLarge,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Release date",
+                              style: theme.textTheme.bodyLarge,
+                            ),
+                            SizedBox(height: 10.sp),
+                            Text(
+                              "${movie.releaseDate}",
+                              style: theme.textTheme.bodyLarge,
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 48,
-                          width: 150.sp,
-                          child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              padding: EdgeInsets.zero,
-                              itemBuilder: (context, index) {
-                                return Center(
-                                    child:
-                                        Text(movie.genres?[index].name ?? ""));
-                              },
-                              separatorBuilder: (context, index) {
-                                return SizedBox(width: 10.sp);
-                              },
-                              itemCount: movie.genres?.length ?? 0),
+                        const SizedBox(width: 100),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Genre",
+                              style: theme.textTheme.bodyLarge,
+                            ),
+                            SizedBox(
+                              height: 48,
+                              width: 150.sp,
+                              child: ListView.separated(
+                                  scrollDirection: Axis.horizontal,
+                                  padding: EdgeInsets.zero,
+                                  itemBuilder: (context, index) {
+                                    return Center(
+                                        child: Text(
+                                            movie.genres?[index].name ?? ""));
+                                  },
+                                  separatorBuilder: (context, index) {
+                                    return SizedBox(width: 10.sp);
+                                  },
+                                  itemCount: movie.genres?.length ?? 0),
+                            ),
+                          ],
                         ),
                       ],
+                    ),
+                    SizedBox(height: 18.sp),
+                    Text(
+                      "Synopsis",
+                      style: theme.textTheme.headlineSmall,
+                    ),
+                    SizedBox(height: 8.sp),
+                    ExpandableText(
+                      movie.overview ?? "",
+                      expandText: 'show more',
+                      collapseText: 'show less',
+                      maxLines: 4,
+                      linkColor: Colors.blue,
+                      style: theme.textTheme.bodyLarge,
+                    ),
+                    SizedBox(height: 18.sp),
+                    Text(
+                      "Related movies",
+                      style: theme.textTheme.headlineSmall,
+                    ),
+                    SizedBox(height: 8.sp),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 18.sp),
+                      child: SizedBox(
+                        height: 165.sp,
+                        child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              final movie = similarMovie[index];
+                              return InkWell(
+                                onTap: () async {
+                                  await controller
+                                      .getMovieDetail(movie.id.toString());
+                                },
+                                child: Container(
+                                  width: 100.sp,
+                                  decoration: BoxDecoration(
+                                      color: Colors.orange.withOpacity(.1),
+                                      borderRadius:
+                                          BorderRadius.circular(15.r)),
+                                  child: Column(
+                                    children: [
+                                      NetworkImageBox(
+                                          url:
+                                              "${ApiUrl.image}${movie.posterPath}",
+                                          radius: 15),
+                                      SizedBox(height: 5.sp),
+                                      Center(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 5.0),
+                                          child: Text(
+                                            movie.originalTitle ?? "",
+                                            maxLines: 2,
+                                            style: theme.textTheme.bodyLarge,
+                                            overflow: TextOverflow.ellipsis,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) {
+                              return SizedBox(width: 30.sp);
+                            },
+                            itemCount: similarMovie.length),
+                      ),
                     ),
                   ],
                 ),
-                SizedBox(height: 18.sp),
-                Text(
-                  "Synopsis",
-                  style: theme.textTheme.headlineSmall,
-                ),
-                SizedBox(height: 8.sp),
-                ExpandableText(
-                  movie.overview ?? "",
-                  expandText: 'show more',
-                  collapseText: 'show less',
-                  maxLines: 4,
-                  linkColor: Colors.blue,
-                  style: theme.textTheme.bodyLarge,
-                ),
-                SizedBox(height: 18.sp),
-                Text(
-                  "Related movies",
-                  style: theme.textTheme.headlineSmall,
-                ),
-                SizedBox(height: 8.sp),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 18.sp),
-                  child: SizedBox(
-                    height: 165.sp,
-                    child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          final movie = similarMovie[index];
-                          return InkWell(
-                            onTap: () async {
-                              await controller
-                                  .getMovieDetail(movie.id.toString());
-                            },
-                            child: Container(
-                              width: 100.sp,
-                              decoration: BoxDecoration(
-                                  color: Colors.orange.withOpacity(.1),
-                                  borderRadius: BorderRadius.circular(15.r)),
-                              child: Column(
-                                children: [
-                                  NetworkImageBox(
-                                      url: "${ApiUrl.image}${movie.posterPath}",
-                                      radius: 15),
-                                  SizedBox(height: 5.sp),
-                                  Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 5.0),
-                                      child: Text(
-                                        movie.originalTitle ?? "",
-                                        maxLines: 2,
-                                        style: theme.textTheme.bodyLarge,
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return SizedBox(width: 30.sp);
-                        },
-                        itemCount: similarMovie.length),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+              ),
+            ],
+          )),
     );
   }
 }
